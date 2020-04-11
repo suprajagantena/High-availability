13360e212889f027526a7916294fec3cb4265bdc
suprajagantena <suprajagantena@gmail.com>
2020-04-11 16:38:45 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,16 @@
-resource "aws_subnet" "main" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "public" {
+  vpc_id     = "aws_vpc.dev.id"
+  cidr_block = "10.0.1.0/16"
 
   tags = {
-    Name = "Main"
+    Name = "devpubsub"
+  }
+}
+resource "aws_subnet" "private1" {
+  vpc_id     = "aws_vpc.dev.id"
+  cidr_block = "10.0.2.0/16"
+
+  tags = {
+    Name = "devprisub"
   }
 }
