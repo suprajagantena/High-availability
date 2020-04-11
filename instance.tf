e5092aed0c383cf95449248a561612eb348868ce
suprajagantena <suprajagantena@gmail.com>
2020-04-11 18:14:11 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,9 +4,9 @@ data "aws_instance" "foo" {
   filter {
     name   = "image-id"
     values = ["ami-ami-0323c3dd2da7fb37d"]
-    availability_zone = us-east-1a
-    security_group = $ 
-    subnet_id = $
+    availability_zone = "us-east-1a"
+    security_group = "${}" 
+    subnet_id = "${aws_subnet.public.id}"
   }
 
   filter {
