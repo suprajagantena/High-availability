404222aef2a476b09dc8bc7e51b99139bb164db0
suprajagantena <suprajagantena@gmail.com>
2020-04-11 17:57:16 -0400

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,16 @@
+data "aws_instance" "foo" {
+  instance_id = "i-instanceid"
+
+  filter {
+    name   = "image-id"
+    values = ["ami-ami-0323c3dd2da7fb37d"]
+    availability_zone = us-east-1a
+    security_group = $ 
+    subnet_id = $
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["instance-name-tag"]
+  }
+}
