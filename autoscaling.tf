57189fb9dd18c34677c8a12e5e7124bdb453bd03
suprajagantena <suprajagantena@gmail.com>
2020-04-11 21:59:36 -0400

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,8 +1,36 @@
+data "aws_ami" "Linux" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["Linux/images/hvm-ssd/Linux-trusty-14.04-amd64-server-*"]
+  }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
+  owners = [""] # Canonical
+}
 
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-example-"
+  image_id      = "${data.aws_ami.Linux.id}"
+  instance_type = "t2.micro"
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-max_size = 2
-min_size = 2
+resource "aws_autoscaling_group" "bar" {
+  name                 = "terraform-asg-example"
+  launch_configuration = "${aws_launch_configuration.as_conf.name}"
+  min_size             = 2
+  max_size             = 2
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
