fe05053b002e9f9e13a20c13b0a684a2ec530c0a
suprajagantena <suprajagantena@gmail.com>
2020-04-14 16:33:31 -0400

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = "${aws_vpc.dev.id}"
+  vpc_id      = "aws_vpc.dev.id"
 
   ingress {
     description = "TLS from VPC"
