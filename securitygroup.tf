acd1a40e4a412e11e4331b5ba8d7a35e186a2af7
suprajagantena <suprajagantena@gmail.com>
2020-04-14 17:39:59 -0400

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -5,13 +5,15 @@ resource "aws_security_group" "sg" {
 
   ingress {
     description = "TLS from VPC"
-    ports   = 443
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["aws_vpc.main.cidr_block"]
   }
 
   egress {
-    ports   = 443
+   from_port   = 0
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
