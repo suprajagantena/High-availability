bb051f99e1f382b1316053e05cf86e16c8b799c9
suprajagantena <suprajagantena@gmail.com>
2020-04-14 16:57:43 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,8 +5,8 @@ data "aws_instance" "pri" {
     name   = "image-id"
     values = ["ami-ami-0323c3dd2da7fb37d"]
     #availability_zone = "us-east-1a"
-    security_group = "aws_security_group.allow_tls.id" 
-    subnet_id = "aws_subnet.private.id"
+   # security_group = "aws_security_group.allow_tls.id" 
+   #subnet_id = "aws_subnet.private.id"
   }
 
   filter {
