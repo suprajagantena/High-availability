a60c7391f0c562b30404d11270eeebda4dfe9704
suprajagantena <suprajagantena@gmail.com>
2020-04-14 18:27:30 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,16 +1,8 @@
-data "aws_instance" "pri" {
-  #instance_id = "i-instanceid"
+resource "aws_instance" "dev" {
+  ami           = "data.aws_ami.Linux.id"
+  instance_type = "t2.micro"
 
-  filter {
-    name   = "image-id"
-    values = ["ami-ami-0323c3dd2da7fb37d"]
-    #availability_zone = "us-east-1a"
-   # security_group = "aws_security_group.allow_tls.id" 
-   #subnet_id = "aws_subnet.private.id"
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["instance-name-tag"]
+  tags = {
+    Name = "HelloWorld"
   }
 }
