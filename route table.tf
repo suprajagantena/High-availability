f796a64de31b01eb611b509085bcae823af625e3
suprajagantena <suprajagantena@gmail.com>
2020-04-15 10:50:28 -0400

    Update route table.tf

--- route table.tf
+++ route table.tf
@@ -7,8 +7,8 @@ resource "aws_route_table" "r" {
 
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = "aws_internet_gateway.main.id"
-  }
+    gateway_id = "aws_internet_gateway.gw.id" 
+    }
 
   tags = {
     Name = "main"
