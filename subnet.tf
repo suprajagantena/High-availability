f43b46119ca673efbd21677e57facd5092da9ce2
suprajagantena <suprajagantena@gmail.com>
2020-04-17 22:01:16 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,10 +1,12 @@
 resource "aws_subnet" "public" {
   vpc_id     = "aws_vpc.dev.id"
   cidr_block = "10.0.1.0/16"
- availability_zone = data.aws_availability_zones.available.names[0,1]
+ availability_zone = data "aws_availability_zones" "example" {
+  all_availability_zones = true
   tags = {
     Name = "devpubsub"
   }
+}
 }
 resource "aws_subnet" "private1" {
   vpc_id     = "aws_vpc.dev.id"
