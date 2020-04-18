7d555115d061b067999f0a92ddb65003e65c8786
suprajagantena <suprajagantena@gmail.com>
2020-04-17 22:24:48 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "dev" {
   ami           = "data.aws_ami.Linux.id"
   instance_type = "t2.micro"
+  available_zone = data.available_zones[0,1]
 
   tags = {
     Name = "HelloWorld"
