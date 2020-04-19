64394623f52e2e6fadfc947d59af3b3f7745efde
suprajagantena <suprajagantena@gmail.com>
2020-04-19 18:09:59 -0400

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -17,3 +17,11 @@ resource "aws_lb" "test" {
     Environment = "dev"
   }
 }
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.dev.id}"
+}
+
