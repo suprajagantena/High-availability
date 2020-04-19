cb94938b6d0b3c9c9639cef16e5b8597289be08c
suprajagantena <suprajagantena@gmail.com>
2020-04-19 18:19:46 -0400

    Update route table.tf

--- route table.tf
+++ route table.tf
@@ -2,11 +2,11 @@ resource "aws_route_table_association" "a" {
   subnet_id      = "aws_subnet.pub.id"
   route_table_id = "aws_route_table.pub.id"
 }
-#resource "aws_route_table" "r" {
-#  vpc_id = "${aws_vpc.dev.id}"
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.dev.id}"
 
-#  route {
- #   cidr_block = "10.0.1.0/24"
+  route {
+  cidr_block = "10.192.0.0/16"
  #   gateway_id = "aws_internet_gateway.gw.id" 
  #   }
 
